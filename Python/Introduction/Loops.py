8006b3ba4189c7d1e8a591275c40288c16422284
Venumadhavchary <venumadhav1125@gmail.com>
2022-12-04 15:45:53 +0530

    First Commit

---
+++ Python/Introduction/Loops.py
@@ -0,0 +1,11 @@
+"""
+Task
+The provided code stub reads and integer, 
+from STDIN. For all non-negative integers ,
+ print .i**2
+
+Code:
+"""
+n = int(input())
+for i in range(0,n):
+    print(i**2)
